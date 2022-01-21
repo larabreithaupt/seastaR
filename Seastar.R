b3b6624bdc7e33db55d3008335c6548c495c98e7
Lara Breithaupt <lara.breithaupt@duke.edu>
2022-01-21 11:38:29 -0500

    fixed error on line 97, more general trees_to_vcv, added get_partial_cov_matrix

--- Seastar.R
+++ Seastar.R
@@ -81,36 +81,41 @@ trees_to_star <- function(genetrees){
 
 new_phylo <- trees_to_star(tree_list)
 
-get_root_node_matrix <- function(genetree){
+get_partial_cov_matrix <- function(genetree){
   
-  node_dists <- list()
+  #Gets a partially filled matrix with covariances from one tree 
   
   tree_height <-max(nodeHeights(genetree))
+  tips = genetree[["tip.label"]]
+  len_tip = length(tips)
   
-  tips = genetree[["tip.label"]] 
-
   tip_combos <- combn(tips, 2) #pairwise combinations of taxa
+  
+  #Initialize matrix
+  genetree_vcv <- matrix(0, 
+                         len_tip, 
+                         len_tip)
+  rownames(genetree_vcv) <- tips
+  colnames(genetree_vcv) <- tips
   
   for(j in 1:length(tip_combos[1,])){ #for each pairwise combo
     
     col <- tip_combos[,j] #current pairwise combo
-    mrca <- fastMRCA(genetree, col) #most recent common ancestor of combo
-    print(mrca)
-    mrca_height <- fastHeight(genetree, col) #height of mrca
+    mrca <- fastMRCA(genetree, col[1], col[2]) #most recent common ancestor of combo
+    mrca_height <- fastHeight(genetree, col[1], col[2]) #height of mrca from root
     
-    root_to_tip_dist = tree_height - mrca_height #distance from root to tip
-    
-    node_dists[[mrca]] = root_to_tip_dist
+    genetree_vcv[col[1], col[2]] <- mrca_height
     
   }
   
-}
-
-root_node_matrix <- get_root_node_matrix(tree_list[[1]])
-
-fill_matrix <- function(genetree){
+  for(m in 1:len_tip){ #Fill the diagonal elements (variance of each species)
+    
+    label <- tips[m]
+    genetree_vcv[label, label] <- tree_height
+    
+  }
   
-  partial_matrix <- matrix()
+  return(genetree_vcv)
   
 }
 
@@ -132,11 +137,22 @@ trees_to_vcv <- function(tree_list) {
                         len_tip)
   rownames(genetree_vcv) <- tips
   colnames(genetree_vcv) <- tips
+  
 
   for(i in 1:(length(tree_list) - 1)){
     
+    gene_freq <- tree_list[[length(tree_list)]][i] #gene tree frequency 
+    
+    partial_matrix <- get_partial_cov_matrix(tree_list[[i]]) #get partial matrix
+    
+    scaled_matrix <- partial_matrix*gene_freq #scale covs
+    scaled_matrix <- scaled_matrix[, order((colnames(scaled_matrix)))] #reorder cols
+    scaled_matrix <- scaled_matrix[order((rownames(scaled_matrix))), ] #reorder ros
+    
+    genetree_vcv <- genetree_vcv + scaled_matrix
+    
   }
-  print(genetree_vcv)
+  
   return(genetree_vcv)  
 }
 
