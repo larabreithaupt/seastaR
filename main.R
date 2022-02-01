4ec7ed5cf0645618c3abe235d45d34ade6f4c571
Mark Hibbins <mhibbins@iu.edu>
2022-02-01 15:56:41 -0500

    Restructured to add modules

---
+++ main.R
@@ -0,0 +1,27 @@
+remove(list=ls())
+library(phytools)
+library(geiger)
+library(tidyverse)
+lib <- modules::use("R")
+
+#Load in files with my paths (Lara - comment these out on your machine)
+tree_list <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
+sptree <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
+
+#tree_list <- lib$utils$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
+#sptree <- lib$utils$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
+
+
+triplet_branch <- lib$sptree_theory$get_internal_branch(c("sp1", "sp2", "sp3"), sptree)
+
+
+#This part isn't working
+triplets <- lib$sptree_theory$get_triplet_branches(sptree)
+final_matrix <- lib$sptree_theory$get_theory_matrix(sptree)
+
+#This works fine
+genetree_vcv <- lib$estimated_trees$trees_to_vcv(tree_list)
+#print(genetree_vcv)
+
+
+
