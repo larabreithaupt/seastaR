774fb07eaa02aa11a8552507527417978dbd44e0
Lara Breithaupt <lara.breithaupt@duke.edu>
2022-02-02 12:29:29 -0500

    built submatrices, need to fill full matrix

--- R/sptree_theory.R
+++ R/sptree_theory.R
@@ -1,5 +1,6 @@
 import(tidyverse)
 import(utils)
+import(stringr)
 
 #Functions for building covariance matrix from input 
 #species tree in coalescent units
@@ -11,10 +12,14 @@ get_internal_branch <- function(triplet, sptree){
   #larger species tree
   
   triplet_mrca <- phytools::findMRCA(sptree, triplet)
+  
+  max_height <- max(phytools::nodeHeights(sptree))
+  triplet_height <- phytools::nodeheight(sptree, triplet_mrca)
+  final_height <- max_height - triplet_height
+  
   tips_pairwise <- combn(triplet, 2)
   
   internal <- 0
-  
   
   sister <- ""
   sister_mrca <- 0
@@ -22,14 +27,11 @@ get_internal_branch <- function(triplet, sptree){
   for(i in 1:length(tips_pairwise[1,])){#for each pairwise combo
     
     combo <- tips_pairwise[,i]
-    
     mrca <- phytools::findMRCA(sptree, combo)
     
     if(mrca > triplet_mrca){
-      
       sister <- combo
       sister_mrca <- mrca
-      
     }
   }
   tree_edge <- sptree[["edge"]]
@@ -39,24 +41,17 @@ get_internal_branch <- function(triplet, sptree){
   
   for(j in 1:length(tree_edge_len)){ #iterating through node traversal
     
-    
     if (counter == sister_mrca){
-      
       break
-      
     }
-    
     else if ((tree_edge[j, 1] == (counter)) && (tree_edge[j, 2] == (counter + 1))){ #traversal from parent node to our MRCA
       
       internal <- internal + tree_edge_len[j] #internal branch length
-      
       counter <- counter + 1
     }
   }
   
-  tree_height <- max(phytools::nodeHeights(sptree))
-  
-  return(list(internal, tree_height, sister))
+  return(list(internal, final_height, sister))
 }
 
 get_triplet_branches <- function(sptree){
@@ -64,7 +59,9 @@ get_triplet_branches <- function(sptree){
   #This function returns a list of internal branch lengths
   #for all triplets contained in the species tree
   
-  triplet_internals <- list()
+  tau <- c()
+  height <- c()
+  triplet_names <- c()
   
   tip_labels <- sptree[["tip.label"]]
   triplets <- combn(tip_labels, 3)
@@ -72,12 +69,11 @@ get_triplet_branches <- function(sptree){
   for(i in 1:length(triplets[1, ])){
     
     internal_branch <- get_internal_branch(triplets[, i], sptree)
+    #print(internal_branch)
     
     len <- internal_branch[1]
-    sisters <- internal_branch[2]
+    sisters <- internal_branch[3]
     triplet <- sisters[[1]]
-    
-    print(sisters)
     
     for (j in 1:length(triplets[, i])){
       
@@ -87,13 +83,16 @@ get_triplet_branches <- function(sptree){
       
     }
     
-    tip_concat <- paste(triplet, collapse = "")
+    tip_concat <- paste(triplet, collapse = "_")
     
-    triplet_internals[tip_concat] = len
+    tau <- append(tau, len)
+    triplet_names <- append(triplet_names, tip_concat)
+    height <- append(height, internal_branch[2])
     
   }
   
-  return(triplet_internals)
+  ret <- as.data.frame(cbind(tau, height, triplet_names))
+  return(ret)
 }
 
 triplet_theory <- function(tau, height, triplet) {
@@ -107,8 +106,9 @@ triplet_theory <- function(tau, height, triplet) {
                              0, 0, 0), nrow = 3, ncol = 3)
   
   # print(triplet)
-  rownames(partial_matrix) <- triplet
-  colnames(partial_matrix) <- triplet
+  names <- strsplit(as.character(triplet), split = "_")[[1]]
+  rownames(partial_matrix) <- names
+  colnames(partial_matrix) <- names
   
   LS <- 1 - exp(-tau)
   ILS <- (1/3)*exp(-tau)
@@ -150,18 +150,21 @@ get_theory_matrix <- function(sptree){
   tips = sptree[["tip.label"]]
   len_tip = length(tips)
   
-  branches <- get_triplet_branches(sptree)
+  branches_df <- get_triplet_branches(sptree)
+  #print(branches_df)
   
   #Initialize Matrix
   theory_matrix <- matrix(0, len_tip, len_tip)
   rownames(theory_matrix) <- tips
   colnames(theory_matrix) <- tips
   
-  for (i in 1:length(branches[1])){
+  for (i in 1:length(branches_df[1, ])){
     
-    sub_matrix <- triplet_theory(branches[1], branches[2], branches[3])
+    sub_matrix <- triplet_theory(as.numeric(branches_df[i, 1]), 
+                                 as.numeric(branches_df[i, 2]), 
+                                 branches_df[i, 3])
     
-    #print(sub_matrix)
+    print(sub_matrix)
   }
   
 }
--- main.R
+++ main.R
@@ -5,11 +5,11 @@ library(tidyverse)
 lib <- modules::use("R")
 
 #Load in files with my paths (Lara - comment these out on your machine)
-tree_list <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
-sptree <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
+#tree_list <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
+#sptree <- lib$tools$parse_input_file("C:/Users/18126/OneDrive - Indiana University/Projects/pruning_alg/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
 
-#tree_list <- lib$utils$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
-#sptree <- lib$utils$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
+tree_list <- lib$tools$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_genetrees_test_input.txt", genetrees = TRUE)
+sptree <- lib$tools$parse_input_file("~/Downloads/Hahn Lab Pruning Algorithm/seastar/seastaR/test_input_files/seastar_sptree_test_input.txt", genetrees = FALSE)
 
 
 triplet_branch <- lib$sptree_theory$get_internal_branch(c("sp1", "sp2", "sp3"), sptree)
