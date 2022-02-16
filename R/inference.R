6021a921600be14b16fc933e67ca438bda79fd59
Lara Breithaupt <lara.breithaupt@duke.edu>
2022-02-16 11:45:11 -0500

    finished Rd files for inference.R

--- R/inference.R
+++ R/inference.R
@@ -1,25 +1,25 @@
 import(MASS)
 
 sigma2_inference <- function(var_covar, trait){
-  
+
   num_taxa <- length(var_covar[, 1])
   if (num_taxa == length(trait)){
-  
+
     one <- c(rep(1, num_taxa))
-  
+
     zhat_root <- (t(one)%*%solve(var_covar)%*%one)%*%(t(one)%*%solve(var_covar)%*%trait)
     sigma2 <- ((t(trait - zhat_root*one))%*%solve(var_covar)%*%(trait - zhat_root*one))/(num_taxa-1)
-    
+
     return(sigma2)
   }
   else{
     stop("ERROR, trait vector is not the length of the number of taxa")
   }
-  
+
 }
 
 simulate_traits <- function(n_traits, var_covar, sigma2){
-  
+
   num_taxa <- length(var_covar[, 1])
   traits <- mvrnorm(n = n_traits, mu = c(rep(0, num_taxa)), Sigma = sigma2*var_covar)
   return(traits)
