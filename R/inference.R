8068ed3f674b1e70d2b132273ffbf679a94891a4
Lara Breithaupt <lara.breithaupt@duke.edu>
2023-03-15 11:54:13 -0400

    Added functions to calculate likelihood surfaces for trait

--- R/inference.R
+++ R/inference.R
@@ -24,3 +24,25 @@ simulate_traits <- function(n_traits, var_covar, sigma2){
   traits <- mvrnorm(n = n_traits, mu = c(rep(0, num_taxa)), Sigma = sigma2*var_covar)
   return(traits)
 }
+
+
+sigma2_likelihood <- function(sigma2, trait, var_covar) {
+
+  one <-c(1, 1, 1)
+  zhat_root <- (t(one)%*%solve(var_covar)%*%one)%*%(t(one)%*%solve(var_covar)%*%trait)
+  top <- exp((-1/2)*((t(trait - zhat_root*one))%*%solve(sigma2*var_covar)%*%(trait - zhat_root*one)))
+  bottom = sqrt(((2*pi)^3)*det(sigma2*var_covar))
+  logL = log(top/bottom)
+
+  return(logL)
+}
+
+trait_likelihood_surface <- function(trait, var_covar, lower_sigma2, higher_sigma2){
+
+  trait_sigma2_vals <- seq(lower_sigma2, higher_sigma2, length.out = 1000)
+  likelihoods <- lapply(trait_sigma2_vals, sigma2_likelihood, trait, var_covar)
+
+  return(unlist(likelihoods))
+
+}
+
